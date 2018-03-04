fe843e77c5e440d20aeb50eca28da81f5aaa7501
Maciej Kubas <maciekkubas@gmail.com>
2018-03-04 12:25:40 +0100

    Zadanie 9.1

---
+++ Zadanie 9.1 Tworzymy Funkcje/js/scripts.js
@@ -0,0 +1,15 @@
+// scripts.js
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(5, 8);
+var triangle3Area = getTriangleArea (12, 3);
+
+function getTriangleArea(a, h)
+{
+  if(a <= 0 || h <= 0) {
+    return('wrong value');
+  }
+return a * h / 2; 
+}
+    
+console.log(getTriangleArea(10, 6));
