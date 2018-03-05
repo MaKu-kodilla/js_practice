deadb638bef2654a78cb0ec3d8dcd43f144c2488
Maciej Kubas <maciekkubas@gmail.com>
2018-03-05 22:58:32 +0100

    modified js

--- Zadanie 9.3 Velociraptor czy Triceratops/js/scripts.js
+++ Zadanie 9.3 Velociraptor czy Triceratops/js/scripts.js
@@ -1,16 +1,16 @@
 // scripts.js
 
 var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-var dinosaur = 'Triceratops is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-var triceratopsUpperCased = dinosaur.replace('Triceratops','Triceratops'.toUpperCase());
+var dinosaur = 'Triceratops';
+var dinosaurUpperCased = dinosaur.toUpperCase();
 
-console.log(triceratopsUpperCased);
+console.log(dinosaurUpperCased);
 
-var velociraptorToDinosaur = text.replace('Velociraptor' , 'Dinosaur');
+var velociraptorToDinosaur = text.replace('Velociraptor' , dinosaurUpperCased);
 
 console.log(velociraptorToDinosaur);
 
-var halfOfText = triceratopsUpperCased.slice(0, triceratopsUpperCased.length/2);
+var halfOfText = velociraptorToDinosaur.slice(0, velociraptorToDinosaur.length/2);
 
 console.log(halfOfText);
 
